26437be5d973646d633352ca2fab13018c2ba215
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-07-24 12:51:43 -0300

    occurrences controller

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -1,2 +1,32 @@
 class OccurrencesController < ApplicationController
+
+  def index
+    @occurrences = Occurrence.all
+  end
+
+  def show
+    @occurrence = Occurrence.find(params[:id])
+  end
+
+  def new
+    @occurrence = Occurrence.new
+  end
+
+  def create
+    @occurrence = Occurrence.new(occurrence_params)
+    @occurrence.user = current_user
+    if @occurrence.save
+      redirect_to occurrences_path, notice: 'Occurrence was successfully created!'
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def occurrence_params
+    params.require(:occurrence).permit(
+      :ship_id, :latitude, :longitude, :date, :time, :ship_origin, :ship_destination, :user_id, :description
+    )
+  end
 end
