dbed5cdef15ff4df660f09fc7b126b426f59ac2d
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-07-24 12:43:10 -0300

    basic configuration done

---
+++ app/javascript/channels/index.js
@@ -0,0 +1,8 @@
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
+
+//import { success } from '..//coordinates';
+//import { coordsArray } from '../packs/coordinates_array';
